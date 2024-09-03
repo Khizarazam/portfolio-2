e0a9cfdf64c1d61d03a6d96f2ec9223ecc3abd5f
Khizarazam <khizarazam143@gmail.com>
2024-09-03 20:25:25 +0500

    seventh commit

--- src/main.jsx
+++ src/main.jsx
@@ -14,7 +14,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
     <HashRouter>
       <Routes>
         <Route path='/' element={<App />}>
-          <Route index element={<About />}/>
+          <Route path='' element={<About />}/>
           <Route path='about' element={<About />}/>
           <Route path='resume' element={<Resume />}/>
           <Route path='projects' element={<Projects />}/>
