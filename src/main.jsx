42af6a72e8d028dac8bcd335f59928db24df4262
Khizarazam <164862318+Khizarazam@users.noreply.github.com>
2024-09-03 20:00:51 +0500

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -14,7 +14,8 @@ ReactDOM.createRoot(document.getElementById('root')).render(
     <HashRouter>
       <Routes>
         <Route path='/' element={<App />}>
-          <Route path='' element={<About />}/>
+          <Route index element={<App />}/>
+          <Route path='/' element={<About />}/>
           <Route path='resume' element={<Resume />}/>
           <Route path='projects' element={<Projects />}/>
           <Route path='contact' element={<Contact />}/>
