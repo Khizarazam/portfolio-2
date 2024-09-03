50e069b4384147aa85cf263c98b960c57f9084a8
Khizarazam <khizarazam143@gmail.com>
2024-09-03 19:36:04 +0500

    second commit

--- src/main.jsx
+++ src/main.jsx
@@ -1,29 +1,26 @@
-import { StrictMode } from 'react'
-import ReactDOM from 'react-dom/client'
-import { BrowserRouter, Routes, Route, RouterProvider, createBrowserRouter, createRoutesFromElements } from 'react-router-dom'
-import App from './App.jsx'
-import './index.css'
-import About from './Components/About.jsx'
-import Projects from './Components/Projects.jsx'
-import Contact from './Components/Contact.jsx'
-import Resume from './Components/Resume.jsx'
-import Home from './Components/Home.jsx'
-
-const router = createBrowserRouter(
-  createRoutesFromElements(
-    <Route path='/' element={<App />}>
-      <Route path='' element={<About />}/>
-      <Route path='about' element={<About />}/>
-      <Route path='resume' element={<Resume />}/>
-      <Route path='projects' element={<Projects />}/>
-      <Route path='contact' element={<Contact />}/>
-    </Route>
-  ),
-  { basename: "/portfolio-2" } // Add this line for GitHub Pages
-)
+import { StrictMode } from 'react';
+import ReactDOM from 'react-dom/client';
+import { HashRouter, Routes, Route } from 'react-router-dom';
+import App from './App.jsx';
+import './index.css';
+import About from './Components/About.jsx';
+import Projects from './Components/Projects.jsx';
+import Contact from './Components/Contact.jsx';
+import Resume from './Components/Resume.jsx';
+import Home from './Components/Home.jsx';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <RouterProvider router={router} />
+    <HashRouter>
+      <Routes>
+        <Route path='/' element={<App />}>
+          <Route index element={<About />}/>
+          <Route path='about' element={<About />}/>
+          <Route path='resume' element={<Resume />}/>
+          <Route path='projects' element={<Projects />}/>
+          <Route path='contact' element={<Contact />}/>
+        </Route>
+      </Routes>
+    </HashRouter>
   </StrictMode>
-)
+);
