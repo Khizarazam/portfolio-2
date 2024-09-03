f8974af88ad1c7df9032740add6d29aab9b11b70
Khizarazam <khizarazam143@gmail.com>
2024-09-03 19:28:52 +0500

    second commit

--- src/main.jsx
+++ src/main.jsx
@@ -1,25 +1,29 @@
 import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import ReactDOM from 'react-dom/client'
+import { BrowserRouter, Routes, Route, RouterProvider, createBrowserRouter, createRoutesFromElements } from 'react-router-dom'
 import App from './App.jsx'
 import './index.css'
 import About from './Components/About.jsx'
 import Projects from './Components/Projects.jsx'
 import Contact from './Components/Contact.jsx'
 import Resume from './Components/Resume.jsx'
+import Home from './Components/Home.jsx'
 
-createRoot(document.getElementById('root')).render(
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path='/' element={<App />}>
+      <Route path='' element={<About />}/>
+      <Route path='about' element={<About />}/>
+      <Route path='resume' element={<Resume />}/>
+      <Route path='projects' element={<Projects />}/>
+      <Route path='contact' element={<Contact />}/>
+    </Route>
+  ),
+  { basename: "/portfolio-2" } // Add this line for GitHub Pages
+)
+
+ReactDOM.createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <BrowserRouter basename="/portfolio-2">
-      <Routes>
-        <Route path="/" element={<App />}>
-          <Route index element={<About />} />
-          <Route path="about" element={<About />} />
-          <Route path="projects" element={<Projects />} />
-          <Route path="Resume" element={<Resume />} />
-          <Route path="contact" element={<Contact />} />
-        </Route>
-      </Routes>
-    </BrowserRouter>
+    <RouterProvider router={router} />
   </StrictMode>
 )
